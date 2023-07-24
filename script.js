d94ab3a3d7ad7fb14a76f7ad7cc66f9f62fdb75b
Vyacheslav <slavik.spiridonow@yandex.ru>
2023-07-24 15:29:15 +0300

    Сделали 3 задачи

---
+++ script.js
@@ -0,0 +1,37 @@
+// задача 1
+
+// const num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log("при переопределении получилось", a);
+
+// const b = 1 + 2 + 3;
+// console.log("cумма чисел равна", b);
+
+// задача 2
+
+// const c = 10, d = 2;
+// let sum = c + d;
+// let difference = c - d;
+// let prod = c * d;
+// let hourly = c / d;
+// console.log(sum, difference, prod, hourly);
+// let sumVariable = sum + difference + prod + hourly;
+// console.log(sumVariable);
+
+// задача 3
+
+// const myName = 'Slava';
+// const surname = 'Spiridonov'; 
+// console.log(myName,surname);
+
+// const partOneName = 'java';
+// const partTwoName = 'script';
+// console.log(partOneName+partTwoName);
+
+// const a = 'hello';
+// const b = 'world';
+
+// console.log(a + ' ' + b);
